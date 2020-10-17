99a3081a7d79ad4915ebcfee974bdcba4a16394c
Alena Gilevskaya <alena@gilevskaya.com>
2020-10-16 21:50:40 -0700

    test test

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
+
+const Thing = () => <div>Thing</div>;
 
 describe('it', () => {
   it('renders without crashing', () => {
